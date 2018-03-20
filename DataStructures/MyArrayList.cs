1a8dfbf3069013b700eff7e90d220af9644fdf45
ChildishhMacbino <connorwilliams.home@gmail.ocm>
2018-03-20 18:10:02 +1100

    Initial

---
+++ DataStructures/MyArrayList.cs
@@ -0,0 +1,76 @@
+﻿using System;
+namespace DataStrucures
+{
+    public class MyArrayList<T>
+    {
+        public int index = 0;
+        public T[] array;
+
+        public MyArrayList()
+        {
+            array = new T[1];
+        }
+
+        public MyArrayList(int size)
+        {
+            array = new T[size];
+        }
+
+        public void add(T value)
+        {
+            if (index >= array.Length) { Resize(array.Length * 2); }
+            array[index] = value;
+            index++;
+        }
+
+        public string get(int Index)
+        {
+            if (!(Index > array.Length))
+            {
+                return array[Index].ToString();
+            }
+            else
+            {
+                return "Index was outside array";
+            }
+        }
+
+        public void Resize(int size)
+        {
+            Array.Resize(ref array, size);
+        }
+
+        public void Remove(T Value)
+        {
+            Remove(Value, false);
+        }
+
+        public void RemoveAll(T Value)
+        {
+            Remove(Value, true);
+        }
+
+        private void Remove(T value, bool all)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array.GetValue(i) != null && array.GetValue(i).Equals(value))
+                {
+                    array[i] = default(T);
+                    Resize(array.Length - 1);
+                    if (!all)
+                    {
+                        return;
+                    }
+
+                }
+            }
+        }
+
+        public int GetSize()
+        {
+            return array.Length;
+        }
+
+    }
+}
---
+++ DataStructures/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DataStrucures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyArrayList<String> arrayList = new MyArrayList<String>();
+            arrayList.add("Connor");
+            for (int i = 0; i < 10; i++)
+            {
+                arrayList.add("Connor");
+            }
+            arrayList.add("Jack");
+            arrayList.add("Chris");
+            arrayList.Remove("Chris");
+            arrayList.RemoveAll("Connor");
+            Console.WriteLine(arrayList.GetSize());
+
+            MyArrayList<int> intArrayList = new MyArrayList<int>(10);
+            intArrayList.add(1);
+            for (int i = 0; i < 10; i++)
+            {
+                intArrayList.add(10);
+            }
+
+        }
+    }
+}
