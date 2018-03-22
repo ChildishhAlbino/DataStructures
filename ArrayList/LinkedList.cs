16d68cc39368cafff56473a313573b1b3cd4de08
ChildishhAlbino <connorwilliams.home@gmail.com>
2018-03-23 01:06:28 +1100

    Finished ArrayList / Added linked lists

---
+++ ArrayList/LinkedList.cs
@@ -0,0 +1,85 @@
+﻿using System;
+namespace DataStrucures
+{
+    public class LinkedList<T>
+    {
+        private Node head;
+        private class Node
+        {
+            public T data;
+
+            public Node next;
+            public Node prev;
+
+            public Node(T data, Node next)
+            {
+                this.data = data;
+                this.next = next;
+            }
+
+            public Node(T data, Node next, Node prev) : this(data, next)
+            {
+                this.prev = prev;
+            }
+        }
+
+        public void Add(T data)
+        {
+            Node node = new Node(data, head);
+            if (head != null) { head.prev = node; }
+            head = node;
+        }
+
+        public void Print()
+        {
+            Node temp;
+            for (temp = head; temp != null; temp = temp.next)
+            {
+                Console.Write($"{temp.data} ");
+            }
+            Console.WriteLine();
+        }
+
+        public void Remove(T value)
+        {
+            Remove(value, false);
+        }
+
+        public void RemoveAll(T value)
+        {
+            Remove(value, true);
+        }
+
+        private void Remove(T value, bool all)
+        {
+            Node temp;
+            for (temp = head; temp != null; temp = temp.next)
+            {
+                Node prev = temp.prev;
+                Node next = temp.next;
+                if (next != null && temp.data.Equals(value))
+                {
+                    if (prev is null) { head = temp.next; }
+                    else
+                    {
+                        prev.next = next;
+                        next.prev = prev;
+                    }
+                    if (!all) { return; }
+                }
+            }
+        }
+
+        public int Length()
+        {
+            int length = 0;
+            Node temp = head;
+            while (temp != null)
+            {
+                length++;
+                temp = head.next;
+            }
+            return length;
+        }
+    }
+}
---
+++ ArrayList/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DataStrucures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> linkedList = new LinkedList<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                linkedList.Add(i);
+                linkedList.Add(10);
+            }
+            
+            //linkedList.Print();
+            linkedList.Remove(5);
+            linkedList.Print();
+            linkedList.Remove(3);
+            linkedList.Print();
+            linkedList.RemoveAll(10);
+            linkedList.Print();
+            linkedList.RemoveAll(2);
+            linkedList.Print();
+            
+
+            Console.ReadLine();
+        }
+    }
+}
